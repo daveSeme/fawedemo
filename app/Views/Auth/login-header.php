8eea272f6e16d87ee157634cd912829d0ff96cf2
David Seme <davidluvai56@gmail.com>
2025-01-29 21:53:11 +0300

    update the index page text colors

--- app/Views/Auth/login-header.php
+++ app/Views/Auth/login-header.php
@@ -8,6 +8,6 @@
 
 echo "<div class=\"height-10 w-100 shadow-lg px-4 bg-brand-gradient\">\r\n  <div class=\"d-flex align-items-center container p-0\">\r\n    <!--<div class=\"page-logo width-mobile-auto m-0 align-items-center justify-content-center p-0 bg-transparent bg-img-none shadow-0 height-9 border-0\"> \r\n     <a href=\"javascript:void(0)\" class=\"page-logo-link press-scale-down d-flex align-items-center\"> \r\n     \t<img src=\"";
 echo base_url();
-echo "/public/img/logo-white.png\" alt=\"M&E Online\" aria-roledescription=\"logo\"> \r\n         <span class=\"page-logo-text mr-1\">M&E Online</span>\r\n      </a> \r\n      </div>-->\r\n      <h1 class=\"system-heading\">FAWE -  Forum for African Women Educationalists</h1>\r\n     <!--<a href=\"#\" class=\"btn-link text-white ml-auto\"> Public Dashboard >> </a> -->\r\n     </div>\r\n</div>\r\n";
+echo "/public/img/logo-white.png\" alt=\"M&E Online\" aria-roledescription=\"logo\"> \r\n         <span class=\"page-logo-text mr-1\">M&E Online</span>\r\n      </a> \r\n      </div>-->\r\n      <h1 class=\"system-heading\">FAWE -  Forum for African Women Educationalists</h1>\r\n     <!--<a href=\"#\" class=\"btn-link text-blue4 ml-auto\"> Public Dashboard >> </a> -->\r\n     </div>\r\n</div>\r\n";
 
 ?>
